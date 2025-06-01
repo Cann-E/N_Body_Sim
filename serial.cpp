7d7d7ef4108846026536ab2af265c614b38b70a3
Can Ercan <159168321+Cann-E@users.noreply.github.com>
2025-06-01 15:57:21 -0500

    Create serial.cpp

---
+++ serial.cpp
@@ -0,0 +1,217 @@
+#include "common.h"
+#include <cmath>
+
+// Apply the force from neighbor to particle
+void apply_force(particle_t& particle, particle_t& neighbor) {
+    // Calculate Distance
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+
+    // Check if the two particles should interact
+    if (r2 > cutoff * cutoff)
+        return;
+
+    r2 = fmax(r2, min_r * min_r);
+    double r = sqrt(r2);
+
+    // Very simple short-range repulsive force
+    double coef = (1 - cutoff / r) / r2 / mass;
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+}
+
+// Integrate the ODE
+void move(particle_t& p, double size) {
+    // Slightly simplified Velocity Verlet integration
+    // Conserves energy better than explicit Euler method
+    p.vx += p.ax * dt;
+    p.vy += p.ay * dt;
+    p.x += p.vx * dt;
+    p.y += p.vy * dt;
+
+    // Bounce from walls
+    while (p.x < 0 || p.x > size) {
+        p.x = p.x < 0 ? -p.x : 2 * size - p.x;
+        p.vx = -p.vx;
+    }
+
+    while (p.y < 0 || p.y > size) {
+        p.y = p.y < 0 ? -p.y : 2 * size - p.y;
+        p.vy = -p.vy;
+    }
+}
+
+
+void init_simulation(particle_t* parts, int num_parts, double size) {
+	// You can use this space to initialize static, global data objects
+    // that you may need. This function will be called once before the
+    // algorithm begins. Do not do any particle simulation here
+}
+
+void simulate_one_step(particle_t* parts, int num_parts, double size) {
+    // Compute Forces
+    for (int i = 0; i < num_parts; ++i) {
+        parts[i].ax = parts[i].ay = 0;
+        for (int j = 0; j < num_parts; ++j) {
+            apply_force(parts[i], parts[j]);
+        }
+    }
+
+    // Move Particles
+    for (int i = 0; i < num_parts; ++i) {
+        move(parts[i], size);
+    }
+}
+
+
+#include "common.h"
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <vector>
+
+// =================
+// Helper Functions
+// =================
+
+// I/O routines
+void save(std::ofstream& fsave, particle_t* parts, int num_parts, double size) {
+    static bool first = true;
+
+    if (first) {
+        fsave << num_parts << " " << size << "\n";
+        first = false;
+    }
+
+    for (int i = 0; i < num_parts; ++i) {
+        fsave << parts[i].x << " " << parts[i].y << "\n";
+    }
+
+    fsave << std::endl;
+}
+
+// Particle Initialization
+void init_particles(particle_t* parts, int num_parts, double size, int part_seed) {
+    std::random_device rd;
+    std::mt19937 gen(part_seed ? part_seed : rd());
+
+    int sx = (int)ceil(sqrt((double)num_parts));
+    int sy = (num_parts + sx - 1) / sx;
+
+    std::vector<int> shuffle(num_parts);
+    for (int i = 0; i < shuffle.size(); ++i) {
+        shuffle[i] = i;
+    }
+
+    for (int i = 0; i < num_parts; ++i) {
+        // Make sure particles are not spatially sorted
+        std::uniform_int_distribution<int> rand_int(0, num_parts - i - 1);
+        int j = rand_int(gen);
+        int k = shuffle[j];
+        shuffle[j] = shuffle[num_parts - i - 1];
+
+        // Distribute particles evenly to ensure proper spacing
+        parts[i].x = size * (1. + (k % sx)) / (1 + sx);
+        parts[i].y = size * (1. + (k / sx)) / (1 + sy);
+
+        // Assign random velocities within a bound
+        std::uniform_real_distribution<float> rand_real(-1.0, 1.0);
+        parts[i].vx = rand_real(gen);
+        parts[i].vy = rand_real(gen);
+    }
+}
+
+// Command Line Option Processing
+int find_arg_idx(int argc, char** argv, const char* option) {
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], option) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int find_int_arg(int argc, char** argv, const char* option, int default_value) {
+    int iplace = find_arg_idx(argc, argv, option);
+
+    if (iplace >= 0 && iplace < argc - 1) {
+        return std::stoi(argv[iplace + 1]);
+    }
+
+    return default_value;
+}
+
+char* find_string_option(int argc, char** argv, const char* option, char* default_value) {
+    int iplace = find_arg_idx(argc, argv, option);
+
+    if (iplace >= 0 && iplace < argc - 1) {
+        return argv[iplace + 1];
+    }
+
+    return default_value;
+}
+
+// ==============
+// Main Function
+// ==============
+
+int main(int argc, char** argv) {
+    // Parse Args
+    if (find_arg_idx(argc, argv, "-h") >= 0) {
+        std::cout << "Options:" << std::endl;
+        std::cout << "-h: see this help" << std::endl;
+        std::cout << "-n <int>: set number of particles" << std::endl;
+        std::cout << "-o <filename>: set the output file name" << std::endl;
+        std::cout << "-s <int>: set particle initialization seed" << std::endl;
+        return 0;
+    }
+
+    // Open Output File
+    char* savename = find_string_option(argc, argv, "-o", nullptr);
+    std::ofstream fsave(savename);
+
+    // Initialize Particles
+    int num_parts = find_int_arg(argc, argv, "-n", 1000);
+    int part_seed = find_int_arg(argc, argv, "-s", 0);
+    double size = sqrt(density * num_parts);
+
+    particle_t* parts = new particle_t[num_parts];
+
+    init_particles(parts, num_parts, size, part_seed);
+
+    // Algorithm
+    auto start_time = std::chrono::steady_clock::now();
+
+    init_simulation(parts, num_parts, size);
+
+#ifdef _OPENMP
+#pragma omp parallel default(shared)
+#endif
+    {
+        for (int step = 0; step < nsteps; ++step) {
+            simulate_one_step(parts, num_parts, size);
+
+            // Save state if necessary
+#ifdef _OPENMP
+#pragma omp master
+#endif
+            if (fsave.good() && (step % savefreq) == 0) {
+                save(fsave, parts, num_parts, size);
+            }
+        }
+    }
+
+    auto end_time = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> diff = end_time - start_time;
+    double seconds = diff.count();
+
+    // Finalize
+    std::cout << "Simulation Time = " << seconds << " seconds for " << num_parts << " particles.\n";
+    fsave.close();
+    delete[] parts;
+}
